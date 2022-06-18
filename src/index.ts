edd1b664e7b383b81d26a12c8543b6841f984768
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2022-06-18 20:39:36 +0300

    feat: different ports for prod and dev

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import server from './server';
 
 env.config();
 
-const port = process.env.PORT || 8000;
+const port = process.env.NODE_ENV === 'production' ? process.env.PORT : 8000;
 
 server.listen(port, () => {
   console.log(`The server is listening at ${port}`);
